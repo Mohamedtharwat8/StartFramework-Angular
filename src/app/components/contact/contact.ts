63cfb5002c1d2fe11399fd642931f7338f02afe8
Mohamed Tharwat <Mohamedtharwat8>
2025-08-06 10:10:03 +0300

    Refactor code structure for improved readability and maintainability

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Star } from '../star/star';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [Star, FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css'
 })
 export class Contact {
+  userName: string = '';
+  userAge: string = '';
+  userEmail: string = '';
+  userPassword: string = '';
 
 }
