091d35d1ad0c1338acb57c9efc3bbb5914528235
Mohamed Tharwat <Mohamedtharwat8>
2025-08-06 09:10:33 +0300

    Refactor code structure for improved readability and maintainability

--- src/app/components/portfolio/portfolio.ts
+++ src/app/components/portfolio/portfolio.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Star } from "../star/star";
 
 @Component({
   selector: 'app-portfolio',
-  imports: [],
+  imports: [Star],
   templateUrl: './portfolio.html',
   styleUrl: './portfolio.css'
 })
 export class Portfolio {
 
+  portfolioSrc: string[] = [
+    '/images/poert1.png',
+    '/images/port3.png',
+    '/images/port2.png',
+    '/images/poert1.png',
+    '/images/port3.png',
+    '/images/port2.png',];
+  flag: boolean = true;
+  modelImg: string = '';
+
+  hideModel(element: EventTarget | null, img: HTMLImageElement): void {
+    if (element === img) return;
+    this.flag = true;
+  }
 }
