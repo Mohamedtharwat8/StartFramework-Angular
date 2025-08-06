190ece532b97fa6bc28ecef3c6f178fab77d9a38
Mohamed Tharwat <Mohamedtharwat8>
2025-08-06 05:40:48 +0300

    feat: add main application routes and components
    - Implemented routing for Home, About, Portfolio, Contact, and Notfound components.
    - Created About component with basic structure and styles.
    - Created Contact component with basic structure and styles.
    - Created Footer component with basic structure and styles.
    - Created Home component with basic structure and styles.
    - Created Navbar component with basic structure and styles.
    - Created Notfound component with basic structure and styles.
    - Created Portfolio component with basic structure and styles.
    - Created Star component with basic structure and styles.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
+import { Portfolio } from './components/portfolio/portfolio';
+import { Contact } from './components/contact/contact';
+import { Notfound } from './components/notfound/notfound';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'about', component: About },
+  { path: 'portfolio', component: Portfolio },
+  { path: 'contact', component: Contact },
+  { path: '**', component: Notfound },
+];
